5632c1131b6a76687828393bb18f8ac6be5e7e03
trm-jpj <116003564+trm-jpj@users.noreply.github.com>
2023-10-12 09:58:45 +0200

    Delete Ledelsesinformation/Status på elbilsudviklingen directory

--- Ledelsesinformation/Status på elbilsudviklingen/render_fil.R
+++
@@ -1,6 +0,0 @@
-
-
-
-rmarkdown::render("Status på elbilsudvikling.rmd", 
-                  output_file = str_glue("Status på elbilsudvikling ({format(Sys.time() %m-% months(1), '%B %Y')}).html"))
-                  
